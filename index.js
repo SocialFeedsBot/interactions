ae06b1fcabf28d407278f728ee93822bf92cd328
jacob <jacobpbond4@gmail.com>
2021-03-30 15:49:57 +0100

    Routes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // INDEX: Connect to the gateway and start up the service.
-const GatewayClient = require('../gateway-client');
+const GatewayClient = require('gateway-client');
 const Interactions = require('./src/Interactions');
 const Logger = require('./src/logger/');
 const config = require('./config');
