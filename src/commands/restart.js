8c1495fbe46a233554ca2d6c69b5b122683c0881
Jacob Bond <jacobpbond4@gmail.com>
2021-12-03 19:31:35 +0000

    fix: optional chaining

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -24,9 +24,9 @@ module.exports = class extends Command {
 
   async run ({ args: { target, id }, user, id: intID, token }) {
     await this.core.rest.api.interactions(intID, token).callback.post(new Command.InteractionResponse()
-      .setContent(`Restarting services with type **${target.value}** (ids: \`${id.value ? id.value.split(',').join('` `') : 'all'}\`)`));
+      .setContent(`Restarting services with type **${target.value}** (ids: \`${id?.value ? id.value.split(',').join('` `') : 'all'}\`)`));
 
-    this.core.gatewayClient.action('restart', { name: target.value, id: id.value ? (id.value === 'all' ? id.value : id.value.split(',')) : undefined }, { restarter: user.id, panel: false });
+    this.core.gatewayClient.action('restart', { name: target.value, id: id?.value ? (id.value === 'all' ? id.value : id.value.split(',')) : undefined }, { restarter: user.id, panel: false });
   }
 
 };
