f9ae3fb604ff79d223e93dc4bc418ea2f2d7db16
Jacob Bond <jacobpbond4@gmail.com>
2021-12-10 20:45:47 +0000

    feat: display update error

--- src/commands/update.js
+++ src/commands/update.js
@@ -14,11 +14,16 @@ module.exports = class extends Command {
 
   async run () {
     const start = Date.now();
-    const result = await this.core.dispatch.commandStore.updateCommandList();
-
-    return new Command.InteractionResponse()
-      .setContent(`Updated **${result.length}** commands in **${Date.now() - start}ms**!`)
-      .setEmoji('check');
+    try {
+      const result = await this.core.dispatch.commandStore.updateCommandList();
+      return new Command.InteractionResponse()
+        .setContent(`Updated **${result.length}** commands in **${Date.now() - start}ms**!`)
+        .setEmoji('check');
+    } catch(e) {
+      return new Command.InteractionResponse()
+        .setContent(`Error while updating commands.\`\`\`js\n${e.stack}\n\`\`\``)
+        .setEmoji('xmark');
+    }
   }
 
 };
