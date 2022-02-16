90437eddb20b9c43cc3a268303f26a26ee3f1957
Jacob Bond <jacobpbond4@gmail.com>
2022-02-16 02:13:42 +0000

    feat: use api v10

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://discord.com/api';
 const CDN_URL = 'https://cdn.discordapp.com';
-const API_VERSION = 9;
+const API_VERSION = 10;
 
 const avatarURL = (userID, hash) => `${CDN_URL}/avatars/${userID}/${hash}.${hash.startsWith('a_') ? 'gif' : 'png'}`;
 const defaultAvatarURL = (discriminator) => `${CDN_URL}/embed/avatars/${discriminator}.png`;
